7a96079c123c10c5d8be439ebf500ba7e25a7ea9
arindampradhan <arindampradhan10@gmail.com>
2018-06-28 08:29:18 +0530

    added redux thunk

--- app/configureStore.js
+++ app/configureStore.js
@@ -5,13 +5,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { fromJS } from 'immutable';
 import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
 import createReducer from './reducers';
 
 export default function configureStore(initialState = {}, history) {
   // Create the store with two middlewares
   // 1. X sagaMiddleware: Makes redux-sagas work
   // 2. routerMiddleware: Syncs the location/URL path to the state
-  const middlewares = [routerMiddleware(history)];
+  const middlewares = [thunk, routerMiddleware(history)];
 
   const enhancers = [applyMiddleware(...middlewares)];
 
