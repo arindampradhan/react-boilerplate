bb9c5c1a2f8d7eba4b0dc9fcccbe358a48a06b4f
Arindam Pradhan <arindampradhan10@gmail.com>
2018-06-28 20:13:23 +0530

    added styleguide

---
+++ styleguide.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  components: [
+    'app/components/Button/index.js',
+    'app/components/H1/index.js',
+    'app/components/List/index.js',
+  ],
+  defaultExample: true,
+  ribbon: {
+    url: 'https://bitbucket.org/rubique_bdf/website_v3/',
+    text: 'Fork on Bitbucket [private]',
+  },
+  title: 'Rubique Styleguide',
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader',
+        },
+      ],
+    },
+  },
+};
